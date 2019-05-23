8bd1a3a4a823e3c175b506d9fb49b244a3c080dc
unknown <cesmaurolet1@gmail.com>
2019-05-23 15:12:49 -0500

    Proyecto Finalizado

---
+++ precache-manifest.6be605e5457db7d452663dc072e951e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24bc7e1e15dc2a73604839114238ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16c82d6f026a8a4f3eee",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "112b1cff6b59fa91467f",
+    "url": "/static/js/2.8959aada.chunk.js"
+  },
+  {
+    "revision": "16c82d6f026a8a4f3eee",
+    "url": "/static/js/main.0a12b7cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
